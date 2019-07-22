90757de1c27d834bd4ed130e0e531a863b3e7b89
Dave Bushnell <dybushnell@gmail.com>
2019-07-22 15:03:26 -0400

    last fix I promise

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,16 +1,21 @@
+
 require_relative "../app/models/actor.rb"
 require_relative "../app/models/movie.rb"
 require_relative "../app/models/car.rb"
 
 def welcome
+  #sleep 1
   logo = Artii::Base.new :font => "slant"
   puts logo.asciify("007")
-  sleep 1
+  #sleep 1
   puts "Welcome to James-Bond-Car-Pedia!"
+  #sleep 1
   puts ""
+  #sleep 1
   puts "The database that includes #{Movie.all.length} films, #{Actor.all.length} actors who have played James Bond, and #{Car.all.length} cars."
+  #sleep 1
   puts ""
-end #end welcome
+end
 
 def menu
   puts ""
@@ -49,19 +54,57 @@ def menu
     puts ""
     menu
   end
-end #end menu
+end
 
 def creation_menu
   puts "Enter an actor"
   input_actor = gets.chomp.rstrip
-  this_actor = c_create_actor(input_actor)
+  this_actor = nil
+  actor_object = Actor.all.find do |actor|
+    actor.name.downcase == input_actor.downcase
+  end
+  this_actor = actor_object
+  if this_actor == nil
+    this_actor = Actor.create(name: input_actor, original_db: false)
+  end
+
   puts "Enter a car"
   input_car = gets.chomp.rstrip
-  this_car = c_create_car(input_car)
+  this_car = nil
+  car_object = Car.all.find do |car|
+    car.name.downcase == input_car.downcase
+  end
+  this_car = car_object
+  if this_car == nil
+    this_car = Car.create(name: input_car, original_db: false)
+  end
+
+  # binding.pry
+
   puts "Enter a movie title"
   input_title = gets.chomp
-  c_create_movie(input_title, this_actor, this_car)
-end #end creation_menu
+  this_title = nil
+  title_object = Movie.all.find do |movie|
+    movie.title.downcase == input_title.downcase
+  end
+  this_title = title_object
+  if this_title == nil
+    puts "Enter the year of the movie"
+    input_year = gets.chomp.to_i
+    puts "Enter the Rotten Tomatoes Critic Score"
+    input_rtcs = gets.chomp.to_i
+    puts "Enter the Rotten Tomatoes Audience Score"
+    input_rtas = gets.chomp.to_i
+    puts "Enter the Box Office Gross in millions (ex: 840 for 840,000,000)"
+    input_actual = gets.chomp.to_i
+
+    this_title = Movie.create(title: input_title, year: input_year, rotten_tomatoes_critic_score: input_rtcs, rotten_tomatoes_audience_score: input_rtas, box_office_actual: input_actual, actor_id: this_actor.id, car_id: this_car.id, original_db: false)
+    puts "Movie added to database!"
+    menu
+  else
+    puts "This title already exists"
+  end
+end
 
 def deletion_menu
   puts ""
@@ -72,11 +115,102 @@ def deletion_menu
   puts "4 - Back to Main Menu"
   input = gets.chomp
   if input == "1"
-    d_delete_actor
+    puts ""
+    new_actor_array = []
+    new_actor_array = Actor.all.select do |actor_instance|
+      actor_instance.original_db == false
+    end
+    if new_actor_array.length == 0
+      puts "No actors can be deleted"
+      deletion_menu
+    else
+      puts "Select an actor to delete:"
+
+      i = 0
+      while i < new_actor_array.length
+        puts "#{i + 1} - #{new_actor_array[i].name}"
+        i += 1
+      end
+      puts "or press any other key to go back"
+
+      input_2 = gets.chomp
+      if input_2.to_i <= 0 || input_2.to_i > new_actor_array.length
+        puts ""
+        puts "invalid input!"
+        deletion_menu
+      else
+        actor = new_actor_array[input_2.to_i - 1].name
+        Actor.all.delete(new_actor_array[(input_2.to_i) - 1])
+        puts ""
+        puts "#{actor} deleted!"
+        deletion_menu
+      end
+    end
   elsif input == "2"
-    d_delete_car
+    puts ""
+    new_car_array = []
+    new_car_array = Car.all.select do |car_instance|
+      car_instance.original_db == false
+    end
+    if new_car_array.length == 0
+      puts "No cars can be deleted"
+      deletion_menu
+    else
+      puts "Select a car to delete:"
+
+      i = 0
+      while i < new_car_array.length
+        puts "#{i + 1} - #{new_car_array[i].name}"
+        i += 1
+      end
+      puts "or press any other key to go back"
+
+      input_2 = gets.chomp
+      if input_2.to_i <= 0 || input_2.to_i > new_car_array.length
+        puts ""
+        puts "invalid input!"
+        deletion_menu
+      else
+        # binding.pry
+
+        car = new_car_array[input_2.to_i - 1].name
+        Car.all.delete(new_car_array[(input_2.to_i) - 1])
+        puts ""
+        puts "#{car} deleted!"
+        deletion_menu
+      end
+    end
   elsif input == "3"
-    d_delete_movie
+    puts ""
+    new_movie_array = []
+    new_movie_array = Movie.all.select do |movie_instance|
+      movie_instance.original_db == false
+    end
+    if new_movie_array.length == 0
+      puts "No movies can be deleted"
+      deletion_menu
+    else
+      puts "Select a movie to delete:"
+
+      i = 0
+      while i < new_movie_array.length
+        puts "#{i + 1} - #{new_movie_array[i].title}"
+        i += 1
+      end
+      puts "or press any other key to go back"
+      input_2 = gets.chomp
+      if input_2.to_i <= 0 || input_2.to_i > new_movie_array.length
+        puts ""
+        puts "invalid input!"
+        deletion_menu
+      else
+        movie = new_movie_array[input_2.to_i - 1].title
+        Movie.all.delete(new_movie_array[(input_2.to_i) - 1])
+        puts ""
+        puts "#{movie} deleted!"
+        deletion_menu
+      end
+    end
   elsif input == "4"
     menu
   else
@@ -108,7 +242,7 @@ def actor_menu
   else
     actor_menu_specifics(input.to_i - 1)
   end
-end #end actor_menu
+end
 
 def actor_menu_specifics(argument)
   puts ""
@@ -125,17 +259,52 @@ def actor_menu_specifics(argument)
   elsif input == 5.to_s
     menu
   elsif input == 1.to_s
-    a_actor_cars(argument)
+    puts ""
+    if Actor.all[argument].cars == []
+      puts "No cars available for this actor (deleted by user)"
+    else
+      puts "#{Actor.all[argument].name} drove "
+      Actor.all[argument].movies.each do |movie|
+        puts "#{movie.car.name} in #{movie.title}"
+      end
+    end
+    sleep 1
+    puts ""
+    actor_menu_specifics(argument)
   elsif input == 2.to_s
-    a_actor_movies(argument)
+    puts ""
+    if Actor.all[argument].movies == []
+      puts "No movies available for this actor (deleted by user)"
+    else
+      puts "#{Actor.all[argument].name} appeared in "
+      Actor.all[argument].movies.each do |movie|
+        puts movie.title
+      end
+    end
+    sleep 1
+    puts ""
+    actor_menu_specifics(argument)
   elsif input == 3.to_s
-    a_highest_rated_movie_and_car(argument)
+    if Actor.all[argument].movies == []
+      puts ""
+      puts "No movies available for this actor (deleted by user)"
+    else
+      a = Actor.all[argument].movies.max_by do |movie|
+        movie.rotten_tomatoes_critic_score
+      end
+      puts ""
+      puts "#{a.title} got #{a.rotten_tomatoes_critic_score}% on Rotten Tomatoes"
+      puts "in which #{Actor.all[argument].name} drove a sweet #{a.car.name}"
+    end
+    sleep 1
+    puts ""
+    actor_menu_specifics(argument)
   else
     puts "invalid input"
     puts ""
     actor_menu_specifics(argument)
   end
-end #end actor_menu_specifics
+end
 
 def movie_menu
   puts ""
@@ -157,7 +326,11 @@ def movie_menu
   else
     movie_menu_specifics(input.to_i - 1)
   end
-end #end movie_menu
+end
+
+def separate_comma(number)
+  comma_number = number.to_s.chars.to_a.reverse.each_slice(3).map(&:join).join(",").reverse
+end
 
 def movie_menu_specifics(argument)
   puts ""
@@ -175,19 +348,54 @@ def movie_menu_specifics(argument)
   elsif input == 6.to_s
     menu
   elsif input == 1.to_s
-    m_show_actor(argument)
+    puts ""
+    if Movie.all[argument].actor == nil
+      puts "Actor missing (deleted by user)"
+    else
+      puts "#{Movie.all[argument].actor.name} played James Bond in #{Movie.all[argument].title}"
+    end
+    sleep 1
+    puts ""
+    movie_menu_specifics(argument)
   elsif input == 2.to_s
-    m_show_box_office_gross(argument)
+    puts ""
+    puts "#{Movie.all[argument].title}'s"
+    puts "Worldwide Box Office Gross (#{Movie.all[argument].year}): "
+    g = Movie.all[argument].box_office_actual.to_i * 1000000
+    puts "$#{separate_comma(g)} USD"
+    puts "Worldwide Box Office Gross (adjusted for 2019): "
+    if Movie.all[argument].year < 1962 || Movie.all[argument].year.to_s == nil
+      puts "data unavailable (invalid year)"
+    else
+      h = g * 8.48 / inflation_hash[0][Movie.all[argument].year.to_s]
+      puts "$#{separate_comma(h.to_i)} USD"
+    end
+    sleep 1
+    puts ""
+    movie_menu_specifics(argument)
   elsif input == 3.to_s
-    m_show_car_featured(argument)
+    puts ""
+    if Movie.all[argument].car == nil
+      puts "Car missing (deleted by user)"
+    else
+      puts "In #{Movie.all[argument].title} James Bond's signature car was the #{Movie.all[argument].car.name}"
+    end
+    sleep 1
+    puts ""
+    movie_menu_specifics(argument)
   elsif input == 4.to_s
-    m_show_rt_score(argument)
+    puts ""
+    puts "#{Movie.all[argument].title}'s Rotten Tomatoes Critic score: #{Movie.all[argument].rotten_tomatoes_critic_score}%"
+    puts "#{Movie.all[argument].title}'s Rotten Tomatoes Audience score: #{Movie.all[argument].rotten_tomatoes_audience_score}%"
+    sleep 1
+    puts ""
+    movie_menu_specifics(argument)
   else
     puts "invalid input"
     puts ""
     movie_menu_specifics(argument)
   end
-end #end movie_menu_specifics
+end
 
 def car_menu
   puts ""
@@ -210,7 +418,7 @@ def car_menu
   else
     car_menu_specifics(input.to_i - 1)
   end
-end #end car_menu
+end
 
 def car_menu_specifics(argument)
   puts ""
@@ -227,14 +435,55 @@ def car_menu_specifics(argument)
   elsif input == 5.to_s
     menu
   elsif input == 1.to_s
-    c_movies(argument)
+    # binding.pry
+    if Car.all[argument].movies == []
+      puts ""
+      puts "No movies available for #{Car.all[argument].name} (deleted by user)"
+    else
+      puts ""
+      puts "The #{Car.all[argument].name} appears in"
+      Car.all[argument].movies.each do |movie|
+        puts movie.title
+      end
+    end
+    sleep 1
+    puts ""
+    car_menu_specifics(argument)
   elsif input == 2.to_s
-    c_actors(argument)
+    if Car.all[argument].actors == []
+      puts ""
+      puts "No actors available for #{Car.all[argument].name} (deleted by user)"
+    else
+      array = []
+      Car.all[argument].actors.each do |actor|
+        array << actor.name
+      end
+      array_2 = array.uniq
+      puts ""
+      puts "The #{Car.all[argument].name} was driven by"
+      puts array_2
+    end
+    sleep 1
+    puts ""
+    car_menu_specifics(argument)
   elsif input == 3.to_s
-    c_highest_rated_movie(argument)
+    if Car.all[argument].movies == []
+      puts ""
+      puts "No movies available for #{Car.all[argument].name} (deleted by user)"
+    else
+      a = Car.all[argument].movies.max_by do |movie|
+        movie.rotten_tomatoes_critic_score
+      end
+      puts ""
+      puts "#{a.title} got #{a.rotten_tomatoes_critic_score}% on Rotten Tomatoes"
+      puts "in which #{a.car.name} was driven"
+    end
+    sleep 1
+    puts ""
+    car_menu_specifics(argument)
   else
     puts "invalid input"
     puts ""
     car_menu_specifics(argument)
   end
-end #car_menu_specifics
+end
